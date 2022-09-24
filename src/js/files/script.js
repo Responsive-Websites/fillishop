d04a0cdebfca1df61b439b282b67714a213f9208
Danila-hub <kontaigabox@mail.ru>
2022-09-24 14:40:35 +0300

    Added top header styles

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,7 @@
 import { isMobile } from './functions.js';
 // Подключение списка активных модулей
 import { flsModules } from './modules.js';
+
+document.querySelector('.top-header__close').addEventListener('click', function (e) {
+  document.querySelector('.header__top').classList.add('_close');
+});
