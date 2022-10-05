e104da64899bdff7fe13caf87412995a64c69df7
Danila-hub <kontaigabox@mail.ru>
2022-10-05 15:20:51 +0300

    Added catalog body block and styles

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -81,5 +81,22 @@ document.addEventListener('click', function (e) {
     document.querySelector('.bottom-header__inner').classList.contains('open-items')
   ) {
     document.querySelector('.bottom-header__inner').classList.remove('open-items');
+  } else if (e.target.closest('.catalog-points__item')) {
+    e.target.classList.toggle('_active');
   }
+});
+
+const pointsShowButton = document.querySelector('.catalog-points__show-button');
+const pointsButtons = document.querySelectorAll('.catalog-points__item');
+
+pointsShowButton.addEventListener('click', function () {
+  pointsButtons.forEach((item) => {
+    if (!item.classList.contains('_active') && !item.classList.contains('_close')) {
+      item.classList.add('_close');
+      pointsShowButton.innerHTML = 'Показать всё';
+    } else if (item.classList.contains('_close') && !item.classList.contains('_active')) {
+      item.classList.remove('_close');
+      pointsShowButton.innerHTML = 'Cвернуть всё';
+    }
+  });
 });
